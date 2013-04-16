f57050a8eaa82477968b0516e2cd0300b154ca9b
delta <delta4d@gmail.com>
2013-04-16 22:34:55 +0800

    fix LL prob, add get & set method

--- DataStructure/BIT.cpp
+++ DataStructure/BIT.cpp
@@ -12,26 +12,37 @@ inline int lowbit(const int x) {
 	return x & -x;
 }
 
-template <class T>
+template <typename T = int>
 struct BIT {
 	int n;
 	T x[MAXN + 1];
 
-	void init(LL _n=MAXN) {
+	void init(int _n=MAXN) {
 		n = _n;
-		memset(x, 0, sizeof(x));
+		memset(x, 0, n*sizeof(T));
 	}
+
 	void add(int p, T off) {
 		for (; p<=n; p+=lowbit(p)) x[p] += off;
 	}
+
 	void add_interval(int l, int r, T off) {
 		add(l, off);
 		add(r+1, -off);
 	}
+
+	// [1..p]
 	T sum(int p) {
-		T tot = 0;
+		T tot = T();
 		for (; p>=1; p-=lowbit(p)) tot += x[p];
 		return tot;
 	}
-};
 
+	T get(int p) {
+		return sum(p) - sum(p-1);
+	}
+
+	void set(int p, T v) {
+		add(p, v-get(v));	
+	}
+};
