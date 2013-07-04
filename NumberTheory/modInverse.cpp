0a74f03f4e75d257290ffc83f708fcd27e7a9fd7
delta <delta4d@gmail.com>
2013-07-05 00:16:35 +0800

    remove template, all args use long long to make it look simpler, change function name as well

--- NumberTheory/modInverse.cpp
+++ NumberTheory/modInverse.cpp
@@ -1,19 +1,21 @@
+typedef long long ll;
+
 // a * x + b * y = gcd(a, b)
-template <class T> T extGcd(T a, T b, T &x, T &y) {
+ll ext_gcd(ll a, ll b, ll &x, ll &y) {
 	if (!b) {
 		x = 1, y = 0;
 		return a;
 	}	
-	T ret = extGcd(b, a%b, x, y);
-	T t = x;
+	ll ret = ext_gcd(b, a%b, x, y);
+	ll t = x;
 	x = y, y = t - a / b * y;
 	return ret;
 }
 
 // find x such that a * x % n = 1, (a, n) = 1
-template <class T> T modInv(T a, T n) {
-	T x, y;
-	extGcd(a, n, x, y);
+ll mod_inv(ll a, ll n) {
+	ll x, y;
+	ext_gcd(a, n, x, y);
 	x %= n;
 	if (x < 0) x += n;
 	return x;
