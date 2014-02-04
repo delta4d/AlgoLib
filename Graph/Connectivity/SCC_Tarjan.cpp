deb94329a7b901b46e7063331d5a5ed90253b287
delta4d <delta4d@gmail.com>
2014-02-04 18:55:23 +0800

    add SCC Tarjan Algo

---
+++ Graph/Connectivity/SCC_Tarjan.cpp
@@ -0,0 +1,44 @@
+/**
+* e[] is graph, nodes are from 0 to n-1
+* num is the number of SCC
+* id[i] means node i is in SCC_{id[i]}
+**/
+
+#include <cstdio>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+const int MAXN = 10000 + 86;
+
+vector <int> e[MAXN];
+int timestamp, num, tail;
+int id[MAXN], dfn[MAXN], low[MAXN], st[MAXN];
+bool instack[MAXN];
+
+void dfs(int x) {
+	instack[x] = true, st[tail++] = x;
+	dfn[x] = low[x] = timestamp++;
+	for (int i=(int)e[x].size()-1; i>=0; --i) {
+		int y = e[x][i];
+		if (instack[y]) low[x] = min(low[x], dfn[y]);
+		if (dfn[y] == -1) {
+			dfs(y);
+			low[x] = min(low[x], low[y]);
+		}
+	}
+	if (dfn[x] == low[x]) {
+		id[x] = num, instack[x] = false;
+		for (int i=st[--tail]; i!=x; instack[i]=false,id[i]=num,i=st[--tail]);
+		++num;
+	}
+}
+
+void tarjan(int n) {
+	timestamp = 0, num = 0, tail = 0;
+	memset(dfn, -1, sizeof(dfn));
+	memset(instack, false, sizeof(instack));
+	for (int i=0; i<n; ++i) if (dfn[i] == -1) dfs(i);
+}
+
